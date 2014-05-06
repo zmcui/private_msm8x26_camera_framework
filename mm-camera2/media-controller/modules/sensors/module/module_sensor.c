5b9a612e82f01fe6fc8fa0197f98576c274d9351
zmcui <ryan.cui87@gmail.com>
2014-05-06 11:20:07 +0800

    Create module_sensor.c

---
+++ mm-camera2/media-controller/modules/sensors/module/module_sensor.c
@@ -0,0 +1,16 @@
+/** module_sensor_find_sensor_subdev: find sensor subdevs
+ *  
+ *  @module_ctrl: sensor ctrl pointer
+ *  return: 0 for success and negative error on failure
+ *  This function finds all sensor subdevs, creates sensor 
+ *  bundle for each sensor subdev and gets init params and 
+ *  subdev info from the subdev
+ **/
+ 
+static void module_sensor_find_sensor_subdev(module_sensor_ctrl_t *module_ctrl)
+{
+    struct media_device_info mdev_info;
+    
+
+
+}
