6fc9431446b4eda5cd3d3e38baaccbd8e18742ed
Ryan <Iryan.cui@gmail.com>
2014-05-09 17:32:38 +0800

    Update server_process.c
    received v4l2_event from kernel and send it to controller

--- mm-camera2/server-imaging/server_process.c
+++ mm-camera2/server-imaging/server_process.c
@@ -43,3 +43,30 @@ boolean server_process_module_init(void)
 	
 	return TRUE;
 }
+
+/** server_process_hal_event:
+ * 	@event: v4l2_event from kernel
+ * 
+ * Process any command received from HAL through kernel
+ * 
+ * Return: process result, action server should take.
+ **/
+serv_proc_ret_t server_process_hal_event(struct v4l2_event *event)
+{
+	serv_proc_ret_t ret;
+	mct_serv_msg_t serv_msg;
+	struct msm_v4l2_event_data *data =
+	(struct msm_v4l2_event_data *)(event->u.data);
+	struct msm_v4l2_event_data *ret_data =
+	(struct msm_v4l2_event_data *)(ret.ret_to_hal.ret_event.u.data);
+	
+	....
+	// czm enter mct_controller.c(mm-camera2/media-controller/mct/controller/)
+	if (mct_controller_proc_serv_msg(&serv_msg) == FALSE) {
+		ret.result = RESULT_FAILURE;
+		goto error_return;
+	}
+	
+	
+	
+}
