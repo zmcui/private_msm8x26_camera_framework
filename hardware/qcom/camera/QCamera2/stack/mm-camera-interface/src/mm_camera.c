e4bfb299101b69d861274998bac974dc73adce0c
Ryan <Iryan.cui@gmail.com>
2014-05-21 16:36:45 +0800

    Create mm_camera.c
    file head description

---
+++ hardware/qcom/camera/QCamera2/stack/mm-camera-interface/src/mm_camera.c
@@ -0,0 +1,7 @@
+ /* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without
+  * modification, are permitted provided that the following conditions are
+  * met:
+  * ....
+  */
