4156ecf7f0bfb877f7a98e0e096cf0d26192bdcf
Ryan <Iryan.cui@gmail.com>
2014-06-06 11:18:21 +0800

    Create cpp_module_events.c
    file head description

---
+++ mm-camera2/media-controller/modules/pproc-new/cpp/cpp_module_events.c
@@ -0,0 +1,4 @@
+/*============================================================
+Copyright (c) 2013-2014 Qualcomm Technologies, Inc. All Rights Reserved.
+Qualcomm Technologies Proprietary and Confidential.
+=============================================================*/
