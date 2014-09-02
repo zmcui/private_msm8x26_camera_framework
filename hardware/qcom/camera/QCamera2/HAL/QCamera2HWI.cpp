c70cea0abff350b89dcadf358a9e4f9c2bb208c7
Ryan <Iryan.cui@gmail.com>
2014-09-02 11:20:20 +0800

    [Memory]Hardware allocate stream buffers
    distinguish types of stream and display choice

--- hardware/qcom/camera/QCamera2/HAL/QCamera2HWI.cpp
+++ hardware/qcom/camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -1,3 +1,115 @@
+/*============================================
+ * FUNCTION : allocateStreamBuf
+ *
+ * DESCRIPTION: allocate stream buffers
+ *
+ * PARAMETERS:
+ *	@stream_type : type of stream
+ *	@size		 : size of buffer
+ *	@stride		 : stride of buffer
+ *	@scanline	 : scanline of buffer
+ *	@bufferCnt	 : [IN/OUT] minimum num of buffers to be allocated.
+ *				   could be modified during allocation if more buffers needed
+ *
+ * RETURN		 : ptr to a memory obj that holds stream buffers.
+ *				   NULL if failed
+ *============================================*/
+ QCameraMemory *QCamera2HardwareInterface::allocateStreamBuf(cam_stream_type_t stream_type,
+ 															 int size,
+ 															 int stride,
+ 															 int scanline,
+ 															 uint8_t &buffercnt)
+{
+	int rc = NO_ERROR;
+	QCameraMemory *mem = NULL;
+	bool bCacheMem = QCAMERA_ION_USE_CACHE;
+	bool bPoolMem = false;
+	char value[PROPERTY_VALUE_MAX];
+	property_get("persist.camera.mem.usepool", value, "1");
+	if(atoi(value) == 1){
+		bPoolMem = true;
+	}
+	
+	// Allocate stream buffer memory object
+	switch(stream_type){
+	case CAM_STREAM_TYPE_PREVIEW:
+		{
+			if(isNoDisplayMode()){
+				mem = new QCameraStreamMemory(mGetMemory,
+						bCacheMem,
+						(bPoolMem) ? &m_memoryPool : NULL,
+						stream_type);
+			}else{
+				cam_dimension_t dim;
+				QCameraGrallocMemory *grallocMemory =
+					new QCameraGrallocMemory(mGetMemory);
+					
+				mParameters.getStreamDimension(stream_type, dim);
+				if(grallocMemory)
+					grallocMemory-setWindowInfo(mPreviewWindow, dim.width,
+						dim.height, stride, scanline, mParameters.getPreviewHalPixelFormat());
+				mem = grallocMemory;
+			}
+		}
+		break;
+	case CAM_STREAM_TYPE_POSTVIEW:
+		{
+			if(isPreviewRestartEnabled() || isNODisplayMode()){
+				mem = new QCameraStreamMemory(mGetMemory, bCacheMem);
+			}else{
+				cam_dimemsion_t dim;
+				QCameraGrallocMemory *grallocMemory =
+					new QCameraGrallocMemory(mGetMemory);
+					
+				mParameters.getStreamDimension(stream_type, dim);
+				if(grallocMemory)
+					grallocMemory-setWindowInfo(mPreviewWindow, dim.width,
+						dim.height, stride, scanline, mParameters.getPreviewHalPixelFormat());
+				mem = grallocMemory;
+			}
+		}
+		break;
+	case CAM_STREAM_TYPE_SNAPSHOT:
+	case CAM_STREAM_TYPE_RAW:
+	case CAM_STREAM_TYPE_METADATA:
+	case CAM_STREAM_TYPE_OFFLINE_PROC:
+		mem = new QCameraStreamMemory(mGetMemory,
+				bCachedMem,
+				(bPoolMem) ? &m_memoryPool : NULL,
+				stream_type);
+		break;
+	case CAM_STREAM_TYPE_VIDEO:
+		{
+			char value[PROPERTY_VALUE_MAX];
+			property_get("persist.camera.mem.usecache", value, "1");
+			if(atoi(value) == 0){
+				bCacheMem = QCAMERA_ION_USE_NOCACHE;	
+			}
+			ALOGD("%s: video buf using cached memory = %d", __func__, bCachedMem);
+			mem = new QCameraVideoMemory(mGetMemory, bCachedMem);
+		}
+		break;
+	case CAM_STREAM_TYPE_DEFAULT:
+	case CAM_STREAM_TYPE_MAX:
+	default:
+		break;
+	}
+	if(!mem){
+		return NULL;
+	}
+	
+	if(bufferCnt > 0){
+		rc = mem->allocate(bufferCnt, size);
+		if(rc < 0){
+			delete mem;
+			return NULL;
+		}
+		bufferCnt = mem->getCnt();
+	}
+	return mem;
+}
+
+
 /*=============================================
  * FUNCTION:	set_parameters
  *
