6d0e363d86a394345ac570bd20fcebd023ac2649
Ryan <Iryan.cui@gmail.com>
2014-10-23 09:30:39 +0800

    [V4L2]mm_camera MM_CHANNEL_EVT_START
    mm_channel_fsm_fn()

--- hardware/qcom/camera/QCamera2/stack/mm-camera-interface/src/mm_camera.c
+++ hardware/qcom/camera/QCamera2/stack/mm-camera-interface/src/mm_camera.c
@@ -69,3 +69,39 @@ int32_t mm_camera_util_s_ctrl(int32_t fd, uint32_t id, int32_t *value)
 	}
 	return (rc >= 0) ? 0 : -1;
 }
+
+/*======================================================
+ * FUNCTION   : mm_camera_start_channel
+ *
+ * DESCRIPTION: start a channel, which will start all streams in the channel
+ *
+ * PARAMETERS :
+ *   @my_obj  : camera object
+ *   @ch_id   : channel handle
+ *
+ * RETURN     : int32_t type of status
+ *              0  -- success
+ *              -1 -- failure
+ *====================================================*/
+int32_t mm_camera_start_channel(mm_camera_obj_t *my_obj,
+				uint32_t ch_id)
+{
+	int32_t rc = -1;
+	mm_channel_t * ch_obj =
+		mm_camera_util_get_channel_by_handler(my_obj, ch_id);
+	
+	if(NULL != ch_obj){
+		pthread_mutex_lock(&ch_obj->ch_lock);
+		pthread_mutex_unlock(&my_obj->cam_lock);
+		
+		rc = mm_channel_fsm_fn(ch_obj,
+							   MM_CHANNEL_EVT_START,
+							   NULL,
+							   NULL);
+	}else{
+		pthread_mutex_unlock(&my_obj->cam_lock);
+	}
+	
+	return rc;
+}
+
